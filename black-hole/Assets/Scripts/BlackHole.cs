d9a8d5c8c25563a6a5ba98a8aa29e9365485b9f7
williambl <william@williambl.com>
2017-11-11 18:50:02 +0000

    Created schwarzchild radius calculator

---
+++ black-hole/Assets/Scripts/BlackHole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour {
+
+        public double mass;
+
+        const double G = 0.0000000006674;
+        const int c = 299792458;
+
+	// Use this for initialization
+	void Start () {
+	    Debug.Log(determine_radius(mass));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+        double determine_radius (double mass_value) {
+           return( (2 * mass_value * G) / Mathf.Pow(c, 2) ); 
+        }
+}
